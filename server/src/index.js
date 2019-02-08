5ad7ea08a992782d5bf64bf24597e1e7f8b2af3b
maelstroms38 <stroms38@gmail.com>
2019-02-08 16:51:27 -0500

    .gitignore fix

---
+++ server/src/index.js
@@ -0,0 +1,37 @@
+const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
+const Query = require('./resolvers/Query')
+const Mutation = require('./resolvers/Mutation')
+const User = require('./resolvers/User')
+const Link = require('./resolvers/Link')
+const Vote = require('./resolvers/Vote')
+const Subscription = require('./resolvers/Subscription')
+
+let links = [{
+  id: 'link-0',
+  url: 'www.howtographql.com',
+  description: 'Fullstack tutorial for GraphQL'
+}]
+
+let idCount = links.length
+
+const resolvers = {
+  Query,
+  Mutation,
+  User,
+  Link,
+  Subscription,
+  Vote,
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: request => {
+    return {
+      ...request,
+      prisma,
+    }
+  },
+})
+server.start(() => console.log(`Server is running on http://localhost:4000`))
