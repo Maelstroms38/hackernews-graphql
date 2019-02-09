eefb6347519ec2247d2159b0eaddb50be192d9c9
Michael Stromer <stroms38@gmail.com>
2019-02-09 14:26:30 -0500

    graphql search auth

--- src/components/CreateLink.js
+++ src/components/CreateLink.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import { Mutation } from 'react-apollo'
+import { FEED_QUERY } from './LinkList'
+import { LINKS_PER_PAGE } from '../constants'
 import gql from 'graphql-tag'
 
 const POST_MUTATION = gql`
@@ -40,7 +42,26 @@ class CreateLink extends Component {
 			            placeholder="The URL for the link"
 			          />
 			    </div>
-        		<Mutation mutation={POST_MUTATION} variables={{ description, url }} onCompleted={() => this.props.history.push('/')}>
+        		<Mutation 
+        			mutation={POST_MUTATION} 
+        			variables={{ description, url }} 
+        			onCompleted={() => this.props.history.push('/')}
+        			onCompleted={() => this.props.history.push('/new/1')}
+				    update={(store, { data: { post } }) => {
+					    const first = LINKS_PER_PAGE
+					    const skip = 0
+					    const orderBy = 'createdAt_DESC'
+					    const data = store.readQuery({
+					      query: FEED_QUERY,
+					      variables: { first, skip, orderBy }
+					    })
+					    data.feed.links.unshift(post)
+					    store.writeQuery({
+					      query: FEED_QUERY,
+					      data,
+					      variables: { first, skip, orderBy }
+					    })
+					  }}>
 				    {postMutation => <button onClick={postMutation}>Submit</button>}
 				</Mutation>
       		</div>
